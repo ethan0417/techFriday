a3188a2dc1e66002149bd6e51cbe528030600931
Ethan Chen <ethan.mail0417@gmail.com>
2016-01-21 13:56:05 +0800

    add api router.

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Ethan' });
+});
+
+router.post('/', function(req, res){
+  res.send('post success');
+})
+
+
+module.exports = router;
